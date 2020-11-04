84179bf90833ea11c1119101cead27154cf53a19
Marko Milanovic <milanovicmarko992@gmail.com>
2020-11-04 14:47:12 +0100

    button group

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          style={{
-            fontSize: 18,
-          }}
-          startIcon={<SaveIcon />}
-          endIcon={<SaveIcon />}
-          size="large"
-          variant="contained"
-          color="primary"
-        >
-          Hello world
-        </Button>
+        <ButtonGroup variant="contained">
+          <Button startIcon={<SaveIcon />} color="primary">
+            Save
+          </Button>
+          <Button startIcon={<DeleteIcon />} color="secondary">
+            Discard
+          </Button>
+        </ButtonGroup>
       </header>
     </div>
   );
